d0b93a8bf26acc2119437477c1dda2480a31dbe1
Teresa Vizzari <57147358+TeresaVizzari@users.noreply.github.com>
2020-11-02 12:55:18 +0100

    Update sdm.r

--- Humans/sdm.r
+++ Humans/sdm.r
@@ -1,4 +1,5 @@
-#./ms 12 2 -t 0.4 -I 10 0 2 0 2 0 2 0 2 2 2 -n 6 5 -n 7 5 -n 8 5 -n 9 5 -n 10 5 -m 6 7 1.52 -m 7 6 8.66 -m 7 8 3.07 -m 8 7 3.76 -m 8 9 0.28 -m 9 8 0.22 -m 9 10 0.51 -m 10 9 0.52 -es 0.25 5 0.998 -ej 0.2501 11 9 -es 0.37 3 0.96 -ej 0.3701 12 10 -es 0.39 1 0.998 -ej 0.3901 13 10 -ej 0.43 9 8 -en 0.4301 8 4 -em 0.4301 7 8 5.73 -em 0.4301 8 7 5.01 -em 0.4301 8 10 5.05 -em 0.432 10 8 7.42 -es 0.44 5 0.989 -ej 0.4401 14 8 -en 0.46 8 0.125 -en 0.46 10 0.125 -ej 0.49 8 7 -ej 0.49 10 7 -es 0.51 5 0.977 -ej 0.5101 15 7 -ej 0.94 5 4 -en 1.05 7 0.125 -ej 1.077 7 6 -ej 3.38 3 2 -ej 4.26 4 2 -ej 5 2 1 -ej 5.5 6 1
+#Single dispersal model
+#Rscript --vanilla ./sdm.r <nchr> <locuslength> <nlociXsim> <rec.rate=1.12e-8> <nsimulations>
 
 samp_int_vec<-function(x=1,y=1:10){
 #x is an integer, y is a vector
@@ -26,22 +27,20 @@ for (i in 1:length(y)){
 return(out)
 }
 
-
-#wrapper script to simulate two population split with asymmetrical gene flow from divergence to the present
-#Rscript --vanilla ./ooa_m1.r <nchr> <locuslength> <nlociXsim> <recrate=1.12e-8> <nsimulations>
 args<-commandArgs(trailingOnly=TRUE)
 ms<-"/opt/software/genetics/msms/bin/msms"
 cpd<-"./"
-mod<-"ooa_m1"
+mod<-"sdm"
 nchr<-as.character(args[1])
 tgen<-29
 mu<-1.25e-8
-recomb<-as.numeric(args[4])
+recomb<-as.numeric(args[4])#recombination rate
 ll<-as.numeric(args[2])#locus length
 nsims<-as.numeric(args[5])#number of ABC simulations
 nloci<-as.numeric(args[3])#loci to simulate in each sim
 out<-paste(mod,"_ll",as.character(ll),"_nl",as.character(nloci),"_r",as.character(recomb),"_nc",nchr,sep="")
-#main param
+
+#Prior distributions parameters as in Malaspinas et al. 2016
 tbot<-2900
 stN<-85735
 stD<-67570
@@ -89,29 +88,26 @@ rG<-1/runif(nsims,min=2,max=100)
 
 tdYG<-sample(40000:145000,nsims,replace=T)
 tGbot<-(tdYG-tbot)
-#tdOA<-sample(35000:tGbot,nsims,replace=T)
 tdOA<-samp_int_vec(35000,tGbot)
 tOAbot<-(tdOA-tbot)
 tdEA<-sample(20000:30000,nsims,replace=T)
 
-
-#taNA<-sample(20000:tdEA,nsims,replace=T)
 taNA<-samp_int_vec(20000,tdEA)
 paNA<-runif(nsims, 10^-3, 10^-1)
 paNA<-1-paNA
-#taDP<-sample(30000:tOAbot,nsims,replace=T)
+
 taDP<-samp_int_vec(30000,tOAbot)
 paDP<-runif(nsims, 10^-3, 10^-1)
 paDP<-1-paDP
-#taARP<-sample(taDP:tOAbot,nsims,replace=T)
+
 taARP<-samp_vec_vec(taDP,tOAbot)
 paARP<-runif(nsims, 10^-3, 10^-1)
 paARP<-1-paARP
-#taNEA<-sample(tdEA:tOAbot,nsims,replace=T)
+
 taNEA<-samp_vec_vec(tdEA,tOAbot)
 paNEA<-runif(nsims, 10^-3, 10^-1)
 paNEA<-1-paNEA
-#taNG<-sample(tdOA:tGbot,nsims,replace=T)
+
 taNG<-samp_vec_vec(tdOA,tGbot)
 paNG<-runif(nsims, 10^-3, 10^-1)
 paNG<-1-paNG
@@ -164,13 +160,15 @@ stdAM<-(tdAM/tgen)/(4*nAR)
 tm1<-(tdEA/tgen)/(4*nAR)
 srec<-4*nAR*(recomb*(ll-1))
 
+#Output: parameters files
 partable<-cbind(nAR,nD,nDR,nN,nNR,nY,nG,nE,nA,nP,nYG,nNNR,nDDR,nDN,nADN,nAM,rYG,rNNR,rDDR,rNDN,rADN,rAM,rP,rEA,rG,tdYG,tGbot,tdOA,tOAbot,tdEA,taNA,paNA,taDP,paDP,taARP,paARP,taNEA,paNEA,taNG,paNG,stN,stD,tdNNR,tdDDR,tdDN,tdADN,tdAM)
 colnames(partable)<-c("nAR","nD","nDR","nN","nNR","nY","nG","nE","nA","nP","nYG","nNNR","nDDR","nDN","nADN","nAM","rYG","rNNR","rDDR","rNDN","rADN","rAM","rP","rEA","rG","tdYG","tGbot","tdOA","tOAbot","tdEA","taNA","paNA","taDP","paDP","taARP","paARP","taNEA","paNEA","taNG","paNG","stN","stD","tdNNR","tdDDR","tdDN","tdADN","tdAM")
 partablescaled<-cbind(tnAR,snD,snDR,snN,snNR,snY,snG,snE,snA,snP,snYG,snNNR,snDDR,snDN,snADN,snAM,srec,stdYG,stGbot,stdOA,stOAbot,stdEA,staNA,staDP,staARP,staNEA,staNG,sstN,sstD,stdNNR,stdDDR,stdDN,stdADN,stdAM,m67,m76,m78,m87,m89,m98,m910,m109,tm1,m1_78,m1_87,m1_810,m1_108)
 write.table(partable,paste(out,".param",sep=""),row.names=F,quote=F,sep="\t")
 write.table(partablescaled,paste(out,".paramscaled",sep=""),row.names=F,col.names=T,quote=F,sep="\t")
+
+#Output summary statistics: FDSS
 i<-1
-#write(paste(ms," ",as.character(6*as.numeric(nchr))," ",as.character(nloci)," -t ",as.character(tnAR[i])," -r ",as.character(srec[i])," ",as.character(ll)," -I 10 ","0"," ",nchr," ","0"," ",nchr," ","0"," ",nchr," ","0"," ",nchr," ",nchr," ",nchr," -n 6 ",as.character(snY[i])," -n 7 ",as.character(snG[i])," -n 8 ",as.character(snE[i])," -n 9 ",as.character(snA[i])," -n 10 ",as.character(snP[i])," -m 6 7 ",as.character(m67[i])," -m 7 6 ",as.character(m76[i])," -m 7 8 ",as.character(m78[i])," -m 8 7 ",as.character(m87[i])," -m 8 9 ",as.character(m89[i])," -m 9 8 ",as.character(m98[i])," -m 9 10 ",as.character(m910[i])," -m 10 9 ",as.character(m109[i])," -es ",as.character(staNA[i])," 5 ",as.character(paNA[i])," -ej ",as.character(staNA[i]+0.000001)," 11 9 -ej ",as.character(stdEA[i])," 9 8 -en ",as.character(stdEA[i])," 8 ",as.character(snE[i])," -em ",as.character(tm1[i])," 7 8 ",as.character(m1_78[i])," -em ",as.character(tm1[i])," 8 7 ",as.character(m1_87[i])," -em ",as.character(tm1[i])," 8 10 ",as.character(m1_810[i])," -em ",as.character(tm1[i])," 10 8 ",as.character(m1_108[i])," -es ",as.character(staDP[i])," 3 ",as.character(paDP[i])," -ej ",as.character(staDP[i]+0.000001)," 12 10 -es ",as.character(staARP[i])," 1 ",as.character(paARP[i])," -ej ",as.character(staARP[i]+0.000001)," 13 10 -es ",as.character(staNEA[i])," 5 ",as.character(paNEA[i])," -ej ",as.character(staNEA[i]+0.000001)," 14 8 -en ",as.character(stOAbot[i])," 8 ",as.character(rEA[i])," -en ",as.character(stOAbot[i])," 10 ",as.character(rP[i])," -ej ",as.character(stdOA[i])," 8 7 -ej ",as.character(stdOA[i])," 10 7 -es ",as.character(staNG[i])," 5 ",as.character(paNG[i])," -ej ",as.character(staNG[i]+0.000001)," 15 7 -ej ",as.character(stdNNR[i])," 5 4 -en ",as.character(stdNNR[i])," 4 ",as.character(snNNR[i])," -en ",as.character(stGbot[i])," 7 ",as.character(rG[i])," -ej ",as.character(stdYG[i])," 7 6 -en ",as.character(stdYG[i])," 6 ",as.character(snYG[i])," -ej ",as.character(stdDDR[i])," 3 2 -en ",as.character(stdDDR[i])," 2 ",as.character(snDDR[i])," -ej ",as.character(stdDN[i])," 4 2 -en ",as.character(stdDN[i])," 2 ",as.character(snDN[i])," -ej ",as.character(stdADN[i])," 2 1 -en ",as.character(stdADN[i])," 1 ",as.character(snDN[i])," -ej ",as.character(stdAM[i])," 6 1 -en ",as.character(stdAM[i])," 1 ",as.character(snAM[i])," | ",cpd,"compute_ss.py -np 6 -nc ",nchr," -w 30 -b 50 -s > ",out,".tab",sep=""), stderr())
 for (i in 1:nsims){
 	#admixture neander > eurasia prima (forward) di admixture arcaica e admixture denisova
 	li1<-paste(ms," ",as.character(6*as.numeric(nchr))," ",as.character(nloci)," -t ",as.character(tnAR[i])," -r ",as.character(srec[i])," ",as.character(ll)," -IT 3 10 ","0 0 0 0 0 0 ",nchr," 0 ",nchr," ",nchr," ",nchr," ",as.character(sstD[i])," 0 ",nchr," 0 0 0 0 0 0 0 0 ",as.character(sstN[i])," 0 0 0 ",nchr," 0 0 0 0 0 0 -n 6 ",as.character(snY[i])," -n 7 ",as.character(snG[i])," -n 8 ",as.character(snE[i])," -n 9 ",as.character(snA[i])," -n 10 ",as.character(snP[i])," -m 6 7 ",as.character(m67[i])," -m 7 6 ",as.character(m76[i])," -m 7 8 ",as.character(m78[i])," -m 8 7 ",as.character(m87[i])," -m 8 9 ",as.character(m89[i])," -m 9 8 ",as.character(m98[i])," -m 9 10 ",as.character(m910[i])," -m 10 9 ",as.character(m109[i])," -es ",as.character(staNA[i])," 5 ",as.character(paNA[i])," -ej ",as.character(staNA[i]+0.000001)," 11 9 -ej ",as.character(stdEA[i])," 9 8 -en ",as.character(stdEA[i])," 8 ",as.character(snE[i])," -em ",as.character(tm1[i])," 7 8 ",as.character(m1_78[i])," -em ",as.character(tm1[i])," 8 7 ",as.character(m1_87[i])," -em ",as.character(tm1[i])," 8 10 ",as.character(m1_810[i])," -em ",as.character(tm1[i])," 10 8 ",as.character(m1_108[i])," -es ",as.character(staDP[i])," 3 ",as.character(paDP[i])," -ej ",as.character(staDP[i]+0.000001)," 12 10 -es ",as.character(staARP[i])," 1 ",as.character(paARP[i])," -ej ",as.character(staARP[i]+0.000001)," 13 10 -es ",as.character(staNEA[i])," 5 ",as.character(paNEA[i])," -ej ",as.character(staNEA[i]+0.000001)," 14 8 -en ",as.character(stOAbot[i])," 8 ",as.character(rEA[i])," -en ",as.character(stOAbot[i])," 10 ",as.character(rP[i])," -ej ",as.character(stdOA[i])," 8 7 -ej ",as.character(stdOA[i])," 10 7 -es ",as.character(staNG[i])," 5 ",as.character(paNG[i])," -ej ",as.character(staNG[i]+0.000001)," 15 7 -ej ",as.character(stdNNR[i])," 5 4 -en ",as.character(stdNNR[i])," 4 ",as.character(snNNR[i])," -en ",as.character(stGbot[i])," 7 ",as.character(rG[i])," -ej ",as.character(stdYG[i])," 7 6 -en ",as.character(stdYG[i])," 6 ",as.character(snYG[i])," -ej ",as.character(stdDDR[i])," 3 2 -en ",as.character(stdDDR[i])," 2 ",as.character(snDDR[i])," -ej ",as.character(stdDN[i])," 4 2 -en ",as.character(stdDN[i])," 2 ",as.character(snDN[i])," -ej ",as.character(stdADN[i])," 2 1 -en ",as.character(stdADN[i])," 1 ",as.character(snDN[i])," -ej ",as.character(stdAM[i])," 6 1 -en ",as.character(stdAM[i])," 1 ",as.character(snAM[i]),sep="")
