44bb65d04cf5349177c6a8ea0ca62fd960ad4d3f
Teresa Vizzari <57147358+TeresaVizzari@users.noreply.github.com>
2020-12-09 11:09:52 +0100

    Update ModelSelection_Malaspinas.r

--- Genes_Vizzari_etal_ALLSCRIPTS/ModelSelection_Malaspinas.r
+++ Genes_Vizzari_etal_ALLSCRIPTS/ModelSelection_Malaspinas.r
@@ -8,10 +8,10 @@ fdir1<-"/ooa_m1/"
 fdir2<-"/ooa_m2/"
 fdir3<-"/observed/malaspinas/"
 
-ll<-as.character(args[1])
-nl<-args[2]
-r<-as.character(args[3])
-chr<-as.character(args[4])
+ll<-as.character(args[1]) #locus length
+nl<-args[2] #Numeber of loci
+r<-as.character(args[3]) #Recombination rate
+chr<-as.character(args[4]) #Number of chromosomes per population
 
 parcomb<-length(ll)*length(nl)*length(r)*length(chr)
 pc<-c()
@@ -25,18 +25,22 @@ for (locusl in ll){
         print(paste("Parameter combination: ",as.character(q),"/",as.character(parcomb),sep=""))
         pc[q]<-paste("ll",locusl,"_nl",nloci,"_r",rec,"_nc",cr,sep="")
         
-         name<-paste(fdir1,"ooa_m1","_ll",locusl,"_nl",nloci,"_r",rec,"_nc",cr,".tab",sep="")
+	      #####Summary statistics SD model
+        name<-paste(fdir1,"ooa_m1","_ll",locusl,"_nl",nloci,"_r",rec,"_nc",cr,".tab",sep="")
         if(file.exists(name)){
           ooa_m1<-read.table(name)
         }else{
           next
         }
+	     #####Summary statistics MD model
         name<-paste(fdir2,"ooa_m2","_ll",locusl,"_nl",nloci,"_r",rec,"_nc",cr,".tab",sep="")
         if(file.exists(name)){
           ooa_m2<-read.table(name)
         }else{
           next
         }
+	      
+	     #####Observed summary statistics
         obs<-list()
         for (i in 1:25){
           name<-paste(fdir3,"obs",as.character(i),"_Malaspinas_2chr_500bp_10k.txt",sep="")
@@ -53,9 +57,14 @@ for (locusl in ll){
 				 
         i<-factor(c(rep(1,nsim-pods),rep(2,nsim-pods)))
         f<-apply(s,2,var)!=0
+	      
+	       #####Reference Table: simulated data
         da<-data.frame(i,s[,f])
-		
+	      
+	       #####Training forest for model selection
         a<-abcrf(i~.,data=da,lda=T,ntree=ntrees,paral=T,ncores=10)
+	      
+	       #####Model selection based on observed data
         c<-predict(object=a,obs=t[,f],training=da,ntree=ntrees,paral=T,paral.predict=T,ncores=10,ncores.predict=10)
 		
         write.table(a$model.rf$confusion.matrix,paste("modsel_malaspinas_ll",locusl,"_nl",nloci,"_r",rec,"_nc",cr,".confMatrix",sep=""))
